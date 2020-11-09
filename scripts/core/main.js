ab4cb91428dc81aa4a5dc8f4e9666ce466d2eed7
Leo Benkel <leo.benkel@gmail.com>
2020-11-09 16:34:17 +0100

    Use navigator

--- scripts/core/main.js
+++ scripts/core/main.js
@@ -1,7 +1,4 @@
 define([
-    // '/scripts/util/local-storage.js',
-    // '/scripts/core/render-skb.js',
-    // '/scripts/core/render-list.js',
     'scripts/core/router.js'
 ],
     function (/*storage, renderSkb, renderList*/router) {
--- scripts/core/router.js
+++ scripts/core/router.js
@@ -1,50 +1,98 @@
 define([
+    '/scripts/core/navigator.js',
 ],
-    function () {
+    function (navigator) {
         // https://verekia.com/requirejs/build-simple-client-side-mvc-app-require-js/
 
+        let skbRoute = { hash: ['skb'], controller: 'render-skb' }
         let routes = [
-            { hash: '#list', controller: 'render-list' },
-            { hash: '#skb', controller: 'render-skb' },
-            { hash: '#about', controller: 'render-about' }
+            { hash: ['list'], controller: 'render-list' },
+            skbRoute,
+            { hash: ['about'], controller: 'render-about' }
         ];
 
-        let defaultRoute = '#list';
-        let currentHash = '';
+        let defaultRoute = 'list';
+        let currentHash = {};
 
-        function loadController(controllerName) {
-            require(['/scripts/controllers/' + controllerName + '.js'], function (controller) {
-                controller.render();
+        let usingEscapedFragment = false;
+
+        let getHash = function () {
+            let r = navigator.get();
+            usingEscapedFragment = r.escaped_fragment;
+            return r.hash;
+        }
+
+        let setCurrentHash = function (controller) {
+            currentHash = {
+                realHash: getHash(),
+                hashObj: controller
+            };
+        }
+
+        let loadController = function (controller, cb) {
+            setCurrentHash(controller);
+            require([`/scripts/controllers/${controller.controller}.js`], function (controllerEngine) {
+                setCurrentHash(controller);
+                if (navigator.isDebug) console.log(`calling controller '${controller.hash}'...`);
+                controllerEngine.render();
+                cb();
             });
         }
 
-        function hashCheck() {
-            if (window.location.hash != currentHash) {
-                let foundRoute = false
-                if (window.location.hash.startsWith("#skb-")) {
+        let hashCheck = function (cb) {
+            let currentHashNotSet = !currentHash.realHash || !currentHash.hashObj;
+            let currentHashIsPageHash = !currentHashNotSet && currentHash.hashObj.hash.includes(getHash());
+            let currentHashIsSKB = !currentHashNotSet && currentHash.realHash.startsWith("skb-");
+            let pageHashIsSKB = getHash().startsWith("skb-");
+            let currentHashIsSameSKB = pageHashIsSKB && currentHashIsSKB && currentHash.realHash == getHash()
+
+            if (currentHashNotSet || !(currentHashIsSameSKB || currentHashIsPageHash)) {
+                let foundRoute = false;
+
+                if (pageHashIsSKB) {
                     foundRoute = true;
-                    loadController('render-skb');
+                    loadController(skbRoute, cb);
+                    return;
                 } else {
                     for (var i = 0, currentRoute; currentRoute = routes[i++];) {
-                        if (window.location.hash == currentRoute.hash) {
+                        if (currentRoute.hash.includes(getHash())) {
                             foundRoute = true
-                            loadController(currentRoute.controller);
+                            loadController(currentRoute, cb);
+                            return;
                         }
                     }
                 }
 
-                if (!foundRoute) {
-                    console.error(`Did not found the route ${window.location.hash}`);
-                    window.location.hash = defaultRoute
-                } else {
-                    currentHash = window.location.hash;
+                if (!foundRoute || !currentHash.realHash) {
+                    navigator.set(defaultRoute);
                 }
+
+                cb();
+                return;
             }
+
+            cb();
+            return;
         }
 
-        function startRouting() {
-            window.location.hash = window.location.hash || defaultRoute;
-            setInterval(hashCheck, 75);
+        let sleepTime = 75
+
+        let loopRenderer = function () {
+            hashCheck(function () {
+                if (usingEscapedFragment) {
+                    usingEscapedFragment = false;
+                    window.history.replaceState({}, document.title, `/#!${getHash()}`);
+                }
+                runRenderer();
+            })
+        }
+
+        let runRenderer = function () {
+            setTimeout(loopRenderer, sleepTime);
+        }
+
+        let startRouting = function () {
+            runRenderer();
         }
 
         return {

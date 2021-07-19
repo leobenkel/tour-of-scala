b8e56cd83c53b8e34c23886a982ce74229c34aa1
Leo Benkel <leo.benkel@gmail.com>
2021-07-19 12:12:06 +0200

    Remove old v1 files

--- _v1/cli/killServers.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-kill -9 $(ps aux | grep "jekyll" | grep -v grep | tr -s ' ' | cut -d ' ' -f 2 ) || echo 'No server running'
--- _v1/scripts/util/configuration.js
+++
@@ -1,8 +0,0 @@
-define(['/scripts/util/find-get-param.js', 'json!/config/config.json'],
-    function (findGetParameter, preConfig) {
-        let d = {
-            debug: findGetParameter('debug', { defaultValue: false }),
-            cache: !findGetParameter('noCache', { defaultValue: false })
-        };
-        return Object.assign(preConfig, d);
-    });

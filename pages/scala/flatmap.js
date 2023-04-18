2351fce9fd5d1c0213a6d0e38e35bb4d8e4b0fa7
Leo Benkel <4960573+leobenkel@users.noreply.github.com>
2023-04-19 01:37:32 +0200

    Update flatmap.js

--- pages/scala/flatmap.js
+++ pages/scala/flatmap.js
@@ -28,8 +28,6 @@ const detailedInfoBox = <>
 
 const description = "Scala Knowledge Bits - Scala flatMap - Periodic exercise to learn bits of knowledge about Scala. Scala flatMap here."
 
-const canonical_url = "https://leobenkel.com/2020/09/skb-scala-flatmap/"
-
 const prevUrl = "random"
 const nextUrl = "curry"
 
@@ -41,7 +39,6 @@ export const pageData = {
   mainInfoBox,
   detailedInfoBox,
   description,
-  canonical_url,
   prevUrl,
   nextUrl
 }
